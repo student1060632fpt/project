d053daeb350503838add6f79d1b8f854731e252f
AnhHuynh248 <huynhhieuanh1314520@gmail.com>
2020-04-05 18:27:37 +0700

    change

--- src/style.js
+++ src/style.js
@@ -1,16 +1,16 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   content: {
     backgroundColor: "red",
     color: "#ffffff",
     "& span": {
-      fontSize: 15
-    }
+      fontSize: 15,
+    },
   },
   title: {
-    fontSize: 50
-  }
+    fontSize: 100,
+  },
 }));
 
 export default useStyles;
