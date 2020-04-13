97409112e2b729e70ba4ef278dedf4d3ac9275ac
student1060632fpt <chuothungbang96@id.tnv.edu.vn>
2020-04-13 09:28:46 +0700

    sua nut

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 import $ from "jquery";
 
 export default class Navbar extends Component {
-  componentDidMount(){
-    $(window).scroll(function() {
+  componentDidMount() {
+    $(window).scroll(function () {
       if ($(document).scrollTop() > 100) {
         $('.navbar').addClass('navbar-home');
       } else {
@@ -15,88 +15,92 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-md  fixed-top">
-        {/* Brand */}
-        <NavLink
-          exact
-          activeClassName="active"
-          className="navbar-brand"
-          to="/"
-        >
-          <img src="./images/logo.png" alt="" /> Cinemax
+      <nav className="navbar navbar-expand-md fixed-top d-print">
+        <div className="container">
+          {/* Brand */}
+          <NavLink
+            exact
+            activeClassName="active"
+            className="navbar-brand"
+            to="/"
+          >
+            <img src="./images/logo.png" alt="" /> Cinemax
         </NavLink>
-        {/* Toggler/collapsibe Button */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavbar"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        {/* Navbar links */}
-        <div
-          className="collapse navbar-collapse"
-          id="collapsibleNavbar"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink
-                exact
-                activeClassName="active"
-                className="nav-link"
-                to="/"
-              >
-                Home
+          {/* Toggler/collapsibe Button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapsibleNavbar"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          {/* Navbar links */}
+          <div
+            className="collapse navbar-collapse flex-row-reverse"
+            id="collapsibleNavbar"
+          >
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/"
+                >
+                  Home
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/about"
-              >
-                About
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/about"
+                >
+                  About
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/list-movie"
-              >
-                ListMovie
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/list-movie"
+                >
+                  ListMovie
                 <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/material"
+                >
+                  Material
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/material"
-              >
-                Material
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/dang-ky"
+                >
+                  Đăng Ký
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/dang-ky"
-              >
-                Đăng Ký
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  className="nav-link form-inline"
+                  to="/dang-nhap"
+                >
+                  <button className="btn button-main">
+                    Đăng nhập
+                  </button>
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                activeClassName="active"
-                className="nav-link"
-                to="/dang-nhap"
-              >
-                Đăng nhập
-              </NavLink>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
