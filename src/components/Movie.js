0a26a600b650c9e42a14046670008448b2e4bd59
student1060632fpt <chuothungbang96@id.tnv.edu.vn>
2020-04-29 14:53:02 +0700

    card

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,25 +4,25 @@ import Tomatoes from './../asset/img/tomatoes.png';
 import Datepic from './../asset/img/cake.png';
 
 export default class Movie extends Component {
-  // componentDidMount() {
-  //   let getParsedDate = (strDate) => {
-  //     var strSplitDate = String(strDate).split(' ');
-  //     var date = new Date(strSplitDate[0]);
-  //     // alert(date);
-  //     var dd = date.getDate();
-  //     var mm = date.getMonth() + 1; //January is 0!
+  // hàm lấy ngày chứ ko lấy giờ
+  getParsedDate = strDate => {
+    var strSplitDate = String(strDate).split(' ');
+    var date = new Date(strSplitDate[0]);
+    // alert(date);
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1; //January is 0!
 
-  //     var yyyy = date.getFullYear();
-  //     if (dd < 10) {
-  //       dd = '0' + dd;
-  //     }
-  //     if (mm < 10) {
-  //       mm = '0' + mm;
-  //     }
-  //     date = dd + "-" + mm + "-" + yyyy;
-  //     return date.toString();
-  //   }
-  // }
+    var yyyy = date.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    date = dd + "-" + mm + "-" + yyyy;
+    return date.toString();
+  }
+
   render() {
     const { movie } = this.props;
     return (
@@ -38,7 +38,7 @@ export default class Movie extends Component {
             <hr />
             <p>
               <img src={Tomatoes} alt="tomatoes" /> {movie.danhGia}
-              <img src={Datepic} alt="Date" /> {movie.ngayKhoiChieu}
+              <img src={Datepic} className="ml-3" alt="Date" /> {this.getParsedDate(movie.ngayKhoiChieu)}
             </p>
           </div>
         </div>
