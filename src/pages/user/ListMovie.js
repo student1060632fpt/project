17cacd8dc0b063513127b445a623494f52c8c340
student1060632fpt <chuothungbang96@id.tnv.edu.vn>
2020-05-29 19:09:09 +0700

    xong list movie

--- src/pages/user/ListMovie.js
+++ src/pages/user/ListMovie.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import * as action from "../../redux/action";
 import { connect } from "react-redux";
-import $ from "jquery";
+import { Link } from "react-router-dom";
+
 
 // source image 
 import City from './../../asset/img/city.png';
@@ -20,7 +21,8 @@ class ListMovie extends Component {
       rap: "",
       day: "",
       firstCinema: "BHDStar",
-      value: ""
+      value: "",
+      maCumRap: ""
     }
   }
 
@@ -35,201 +37,79 @@ class ListMovie extends Component {
     //lấy cinema
     this.props.getCinema();
 
-    //dropdown
-    $('select').each(function () {
-      var $this = $(this), numberOfOptions = $(this).children('option').length;
 
-      $this.addClass('select-hidden');
-      $this.wrap('<div class="select"></div>');
-      $this.after('<div class="select-styled"></div>');
-
-      var $styledSelect = $this.next('div.select-styled');
-      $styledSelect.text($this.children('option').eq(0).text());
-
-      var $list = $('<ul />', {
-        'class': 'select-options'
-      }).insertAfter($styledSelect);
-
-      for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-          text: $this.children('option').eq(i).text(),
-          rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-      }
-
-      var $listItems = $list.children('li');
-
-      $styledSelect.click(function (e) {
-        e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function () {
-          $(this).removeClass('active').next('ul.select-options').hide();
-        });
-        $(this).toggleClass('active').next('ul.select-options').toggle();
-      });
-
-      $listItems.click(function (e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-      });
-
-      $(document).click(function () {
-        $styledSelect.removeClass('active');
-        $list.hide();
-      });
-
-    });
   }
 
   renderHTMLTheater = () => {
     const { theater } = this.props;
-    // console.log(theater);
 
     if (theater) {
       return theater.map(theaterItem => {
-        let theaterName = theaterItem.tenHeThongRap;
-        // console.log(theaterName);
-        let doDaiChuoi = theaterName.length;
-        let start = theaterName.length + 3;
-        return theaterItem.lstCumRap.map((th, index) => {
-          let rapChuaCat = th.tenCumRap;
-          let length = rapChuaCat.length - doDaiChuoi;
-          let rap = rapChuaCat.substr(start, length);
-          // console.log(rap);
-          return (
-            <div key={index} className="row row-theater">
-              <div className="col-sm-4">
-                <div key={index} className="row theater" >
-                  <div className="col-sm-2">
-                    <a href="" className="theater-map"><i className="fa fa-map-marker" aria-hidden="true"></i></a>
-                  </div>
-                  <div className="col-sm-10">
-                    <a href="">
-                      {rap}
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-8 col-ticket">
-                <div className="row text-center div-ticket">
-                  {/* {this.renderTicket(movie.lichChieu[indexBefore].thongTinRap.maRap)} */}
-                </div>
+        return theaterItem.lstCumRap.map((th) => {
+          // console.log(th);
+          if (th.maCumRap === this.state.maCumRap) {
 
-              </div>
-            </div>
-          );
+            return th.danhSachPhim.map((phim, index) => {
+              return (
+                <div key={index} className="row row-theater">
+                  <div className="col-sm-4">
+                    <Link className="theater-img" to={`/detail/${phim.maPhim}`}>
+                      <img src={phim.hinhAnh} alt={phim.tenPhim} />
+                    </Link>
+                  </div>
+                  <div className="col-sm-8 col-ticket">
+                    <div className="row text-center div-ticket">
+                      {/* {this.renderTicket(movie.lichChieu[indexBefore].thongTinRap.maRap)} */}
+                      {this.renderHTMLTicket(phim.maPhim)}
+                    </div>
+                  </div>
+                </div>
+              );
+
+            })
+          }
+
         });
       })
     }
   }
+  renderHTMLTicket = maPhimCho => {
+    // console.log(maPhimCho);
+    var moment = require('moment'); // require
 
-  renderTheater = () => {
-    const { movie } = this.props;
-    // console.log(movie);
+    const { theater } = this.props;
 
-    if (movie.lichChieu) {
+    if (theater) {
+      return theater.map(theaterItem => {
+        return theaterItem.lstCumRap.map((th) => {
+          return th.danhSachPhim.map((phim) => {
+            let maPhimXet = phim.maPhim;
 
-      return movie.lichChieu.map((item, index) => {
-        let rap = item.thongTinRap.tenCumRap;
-        if (index > 0) {
-          let indexBefore = index - 1;
-          let rapBefore = movie.lichChieu[indexBefore].thongTinRap.tenCumRap;
-          if (rap === rapBefore) {
-            // console.log("noo");
-            //nếu date bằng date lần trước thì sẽ ko hiển thị
-            return;
-          } else {
-            // console.log("index = " + index + "; indexBefore = " + indexBefore + "; date = " + rap + " dateBefore = " + rapBefore);
-
-            return (
-              <div key={index} className="row row-theater">
-                <div className="col-sm-4">
-                  <div key={index} className="row theater" onClick={() => { this.handleRap(movie.lichChieu[indexBefore].thongTinRap.maRap) }}>
-                    <div className="col-sm-2">
-                      <a href="" className="theater-map"><i className="fa fa-map-marker" aria-hidden="true"></i></a>
-
+            if (maPhimCho === maPhimXet) {
+              console.log(phim.tenPhim);
+              return phim.lstLichChieuTheoPhim.map((item, index) => {
+                let stateday = moment(this.state.day).format("MM/DD");
+                let thisday = moment(item.ngayChieuGioChieu).format("MM/DD");
+                if (stateday === thisday) {
+                  return (
+                    <div className="col-sm-3">
+                      {/* và đây là nút chọn ghế hehe  */}
+                      <button key={index} className="btn-ticket" data-toggle="modal" data-target="#seatModal">
+                        {moment(item.ngayChieuGioChieu).format("HH:mm")}
+                      </button>
                     </div>
-                    <div className="col-sm-10">
+                  );
+                }
+              })
 
-                      <a href="">
-                        {rapBefore}
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-8 col-ticket">
-                  <div className="row text-center div-ticket">
-                    {this.renderTicket(movie.lichChieu[indexBefore].thongTinRap.maRap)}
-                  </div>
-
-                </div>
-              </div>
-            );
-          }
-        }
-      });
+            }
+          })
+        })
+      })
     }
   }
 
-  // render html ra vé xem phim với suất chiếu, nhớ hiện modal chuyển trang ticket
-  renderTicket = (maRap) => {
-    var moment = require('moment'); // require
 
-    // console.log(maRap);
-    // nó sẽ nhận lần lượt 5 cái rạp
-    const { movie } = this.props;
-    return movie.lichChieu.map((item, index) => {
-      let maRapXet = item.thongTinRap.maRap;
-      if (maRap === maRapXet) {
-        let stateday = moment(this.state.day).format("MM/DD");
-        let thisday = moment(item.ngayChieuGioChieu).format("MM/DD");
-        if (stateday === thisday) {
-          return (
-            <div className="col-sm-3">
-              {/* và đây là nút chọn ghế hehe  */}
-              <button key={index} className="btn-ticket" data-toggle="modal" data-target="#seatModal">
-                {moment(item.ngayChieuGioChieu).format("HH:mm")}
-              </button>
-            </div>
-          );
-        }
-      }
-    });
-  }
-
-  renderSlider = () => {
-    var moment = require('moment'); // require
-    const { movie } = this.props;
-
-    if (movie.lichChieu) {
-      return movie.lichChieu.map((item, index) => {
-        // let chooseRap = item.thongTinRap.maRap;
-        // if (this.state.rap === chooseRap) {
-        // console.log(chooseRap);
-        let date = new Date(item.ngayChieuGioChieu).toLocaleDateString();
-        if (index > 0) {
-          let indexBefore = index - 1;
-          let dateBefore = new Date(movie.lichChieu[indexBefore].ngayChieuGioChieu).toLocaleDateString();
-          if (date === dateBefore) {
-            // console.log("noo");
-            //nếu date bằng date lần trước thì sẽ ko hiển thị
-          } else {
-            // console.log("index = " + index + "; indexBefore = " + indexBefore + "; date = " + date + " dateBefore = " + dateBefore);
-            return (
-              <div className="text-center" style="outline: none !important">
-                <button key={index} className="btn-normal" onClick={() => { this.chooseDay(movie.lichChieu[indexBefore].ngayChieuGioChieu) }}>
-                  {moment(movie.lichChieu[indexBefore].ngayChieuGioChieu).format("MM/DD")}
-                </button>
-              </div>
-            );
-          }
-        }
-        // }
-      });
-    }
-  };
 
   renderDate = () => {
     var moment = require('moment'); // require
@@ -238,49 +118,95 @@ class ListMovie extends Component {
     if (theater) {
       return theater.map(theaterItem => {
         return theaterItem.lstCumRap.map(cumRapItem => {
-          return cumRapItem.danhSachPhim.map(phimItem => {
-            return phimItem.lstLichChieuTheoPhim.map(lichChieuItem => {
-              let index = Math.random() * 1000;
-              // console.log(lichChieuItem.ngayChieuGioChieu);
-              return (
-                <div className="text-center" style="outline: none !important">
-                  <button key={index} className="btn-normal">
-                    {moment(lichChieuItem.ngayChieuGioChieu).format("MM/DD")}
-                  </button>
-                </div>
-              );
+          // console.log(cumRapItem);
+          let cumRapXet = cumRapItem.maCumRap;
+          if (cumRapXet === this.state.maCumRap) {
+            return cumRapItem.danhSachPhim.map(phimItem => {
+              return phimItem.lstLichChieuTheoPhim.map((lichChieuItem, index) => {
+                // let dayNow = moment(lichChieuItem.ngayChieuGioChieu).format("MM/DD");
+                // let indexBefore = index -1;
+                // let dayBefore = phimItem.lstLichChieuTheoPhim[indexBefore].ngayChieuGioChieu;
+                // console.log(dayBefore);
+                // if(dayNow === dayBefore){
+
+                // }
+                return (
+                  <div className="text-center" style="outline: none !important">
+                    <button key={index} className="btn-normal" onClick={() => { this.chooseDay(lichChieuItem.ngayChieuGioChieu) }}>
+                      {moment(lichChieuItem.ngayChieuGioChieu).format("MM/DD")}
+                    </button>
+                  </div>
+                );
+              });
             });
-          });
+          }
         })
       })
     }
   }
 
   chooseDay = day => {
-    // console.log(day);
+    console.log(day);
     this.setState({ day })
   }
 
-  //hàm ko thực hiện dc
-  renderCinema = () => {
-    let { cinema } = this.props;
-    if (cinema) {
-      return cinema.map((item, index) => {
-        // console.log(item.maHeThongRap);
-        return (
-          <option key={index} value={item.maHeThongRap}>
-            {item.tenHeThongRap}
-          </option>
-        );
+  renderOptionTheater = () => {
+    let { theater } = this.props;
+    // console.log(theater);
+    if (theater) {
+      return theater.map(mainTheater => {
+        let theaterName = mainTheater.tenHeThongRap;
+
+        let doDaiChuoi = theaterName.length;
+        let start = 0;
+        if (mainTheater.maHeThongRap === "BHDStar" || mainTheater.maHeThongRap === "CGV" || mainTheater.maHeThongRap === "MegaGS") {
+          start = doDaiChuoi + 3;
+        } else if (mainTheater.maHeThongRap === "CineStar") {
+          doDaiChuoi = theaterName.length - 2;
+          start = theaterName.length - 2;
+        }
+        return mainTheater.lstCumRap.map((item, index) => {
+          let rapChuaCat = item.tenCumRap;
+
+          let length = rapChuaCat.length - doDaiChuoi;
+          let rap = rapChuaCat.substr(start, length);
+          if (mainTheater.maHeThongRap === "BHDStar" || mainTheater.maHeThongRap === "CGV" || mainTheater.maHeThongRap === "MegaGS" || mainTheater.maHeThongRap === "CineStar") {
+            return (
+              <option
+                value={item.maCumRap}
+                key={index}
+              >
+                {rap}
+              </option>
+            );
+          } else {
+            return (
+              <option
+                value={item.maCumRap}
+                key={index}
+              >
+                {item.tenCumRap}
+              </option>
+            );
+          }
+        })
       })
     }
   }
 
+  //renderMovie
+  renderMovie = event => {
+    let maCumRap = event.target.value;
+    // console.log(phim);
+    this.setState({ maCumRap })
+  }
+
+  // option select cinema 
   selectCinema = event => {
     let firstCinema = event.target.value;
-    console.log(firstCinema);
-    
-    
+    // console.log(firstCinema);
+    // this.setState({firstCinema})
+    this.props.getTheater(firstCinema);
   }
 
   componentWillUnmount() {
@@ -288,12 +214,15 @@ class ListMovie extends Component {
     this.props.resetDetailMovie();
   }
 
+  renderTheaterName = () => {
+    if (this.props.theater) {
+      return this.props.theater.map(index => {
+        return index.tenHeThongRap;
+      })
+    }
+  }
   render() {
-    // const { theater } = this.props;
-    // console.log(theater);
-
-    const { cinema } = this.props;
-    // console.log(cinema);
+    const { theater } = this.props;
 
     const settings1 = {
       dots: false,
@@ -334,7 +263,7 @@ class ListMovie extends Component {
         <div className="banner-detail">
           <img src={Banner} alt="banner-img" className="banner-detail-img" />
           <div className="banner-text text-center">
-            <h2>BHD Star</h2>
+            <h2 className="text-uppercase">{this.renderTheaterName()}</h2>
             <p>Cheapest cinema ever</p>
           </div>
         </div>
@@ -342,19 +271,17 @@ class ListMovie extends Component {
         <div className="theater-option">
           <div className="container">
             <div className="row form-inline text-center">
-              <div className="form-group my-select col-sm-3">
+              <div className="form-group col-sm-3">
                 <div className="thumb">
                   <img src={City} className="mr-2" alt="date" />
                 </div>
                 <span className="type">Cinema</span>
-                <select 
-                className="select-bar" 
-                value={this.state.value}
-                onChange={this.selectCinema}>
-
-                  {/* {this.renderCinema()} */}
-                  
-                  <option value="BHDStar">BHD Star</option>
+                <select
+                  className="custom-select"
+                  // value={this.state.value}
+                  onChange={this.selectCinema}
+                >
+                  <option value="BHDStar" active>BHD Star</option>
                   <option value="CGV">CGV</option>
                   <option value="CineStar">CineStar</option>
                   <option value="Galaxy">Galaxy Cinema</option>
@@ -363,35 +290,33 @@ class ListMovie extends Component {
                 </select>
               </div>
 
-              <div className="form-group my-select col-sm-3">
+              <div className="form-group col-sm-3">
                 <div className="thumb">
                   <img src={Cinema} className="mr-2" alt="date" />
                 </div>
                 <span className="type">Location</span>
-                <select className="select-bar">
-                  <option value="26-12-19">Quoc Thanh</option>
-                  <option value="26-12-19">Thao Dien</option>
-                  <option value="26-12-19">Le Van Viet</option>
+                <select className="custom-select" onChange={this.renderMovie}>
+                  <option value="">Choose one</option>
+                  {this.renderOptionTheater()}
                 </select>
               </div>
-              <div className="form-group my-select col-sm-3">
+              <div className="form-group col-sm-3">
                 <div className="thumb">
                   <img src={DateImg} className="mr-2" alt="date" />
                 </div>
-                <span className="type">Date</span>
-                <select className="select-bar">
-                  <option value="26-12-19">23/10/2020</option>
-                  <option value="26-12-19">24/10/2020</option>
-                  <option value="26-12-19">25/10/2020</option>
-                  <option value="26-12-19">26/10/2020</option>
+                <span className="type">City</span>
+                <select className="custom-select">
+                  <option value="26-12-19">Ho Chi Minh</option>
+                  <option value="26-12-19">Da Nang</option>
+                  <option value="26-12-19">Ha Noi</option>
                 </select>
               </div>
-              <div className="form-group my-select col-sm-3">
+              <div className="form-group col-sm-3">
                 <div className="thumb">
                   <img src={Exp} className="mr-2" alt="date" />
                 </div>
                 <span className="type">Experience</span>
-                <select className="select-bar">
+                <select className="custom-select">
                   <option value="26-12-19">Subtitle</option>
                   <option value="26-12-19">Present</option>
                   <option value="26-12-19">Subtitle 3D</option>
@@ -409,7 +334,6 @@ class ListMovie extends Component {
           </Slider>
           <div className="container-theater">
             {this.renderHTMLTheater()}
-            {/* {this.renderTheater()} */}
           </div>
         </div>
 
